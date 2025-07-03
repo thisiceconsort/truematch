4dde5dac6d203c5275fb4ce604e968557832390f
Mìlàájé <iceconsort@gmail.com>
2025-07-03 20:44:49 +0100

    Update texp16cereawbsjwiefbbwkxoweieeifjdbdbfjskdkfkwbehfivkgnwncbdjwksonsbwbcifirngkslgkemdjfkwkfkfnfjdjfjfjejfjrjrjfjfjfjkekrjrjrjrdkdkdkdddndnejdjfjfjwififjjncas.js

--- texp16cereawbsjwiefbbwkxoweieeifjdbdbfjskdkfkwbehfivkgnwncbdjwksonsbwbcifirngkslgkemdjfkwkfkfnfjdjfjfjejfjrjrjfjfjfjkekrjrjrjrdkdkdkdddndnejdjfjfjwififjjncas.js
+++ texp16cereawbsjwiefbbwkxoweieeifjdbdbfjskdkfkwbehfivkgnwncbdjwksonsbwbcifirngkslgkemdjfkwkfkfnfjdjfjfjejfjrjrjfjfjfjkekrjrjrjrdkdkdkdddndnejdjfjfjwififjjncas.js
@@ -2,7 +2,7 @@
 // In a real application, these would be stored and managed on a secure backend.
 // This array holds objects of registered users and their access codes.
 let simulatedUserDatabase = [
-    { name: "John Doe", email: "john.doe@example.com", phone: "+2348012345678", country: "Nigeria", accessCode: "1234JOH", unlockedVideos: {} },
+    { name: "John Doe", email: "john.doe@example.com", phone: "+2348012345678", country: "Nigeria", accessCode: "3214JOH", unlockedVideos: {} },
     { name: "Jane Smith", email: "jane.smith@example.com", phone: "+15551234567", country: "United States", accessCode: "5678JAS", unlockedVideos: {} },
     { name: "Demo User", email: "demo@example.com", phone: "+1234567890", country: "United States", accessCode: "DEMOABC", unlockedVideos: {} }, // Special demo user
     { name: "Sam Esom", email: "orjisamuel19@gmail.com", phone: "+2347067411418", country: "Nigeria", accessCode: "2559SOM", unlockedVideos: {} },
