123b8cb7bf07c4ee41226f87f1dbcc443e2697f6
Mìlàájé <iceconsort@gmail.com>
2025-07-05 07:34:46 +0100

    Update texp16cereawbsjwiefbbwkxoweieeifjdbdbfjskdkfkwbehfivkgnwncbdjwksonsbwbcifirngkslgkemdjfkwkfkfnfjdjfjfjejfjrjrjfjfjfjkekrjrjrjrdkdkdkdddndnejdjfjfjwififjjncas.js

--- texp16cereawbsjwiefbbwkxoweieeifjdbdbfjskdkfkwbehfivkgnwncbdjwksonsbwbcifirngkslgkemdjfkwkfkfnfjdjfjfjejfjrjrjfjfjfjkekrjrjrjrdkdkdkdddndnejdjfjfjwififjjncas.js
+++ texp16cereawbsjwiefbbwkxoweieeifjdbdbfjskdkfkwbehfivkgnwncbdjwksonsbwbcifirngkslgkemdjfkwkfkfnfjdjfjfjejfjrjrjfjfjfjkekrjrjrjrdkdkdkdddndnejdjfjfjwififjjncas.js
@@ -3,8 +3,6 @@
 // This array holds objects of registered users and their access codes.
 let simulatedUserDatabase = [
     { name: "John Doe", email: "john.doe@example.com", phone: "+2348012345678", country: "Nigeria", accessCode: "3214JOH", unlockedVideos: {} },
-    { name: "Jane Smith", email: "jane.smith@example.com", phone: "+15551234567", country: "United States", accessCode: "5678JAS", unlockedVideos: {} },
-    { name: "Demo User", email: "demo@example.com", phone: "+1234567890", country: "United States", accessCode: "DEMOABC", unlockedVideos: {} }, // Special demo user
     { name: "Sam Esom", email: "orjisamuel19@gmail.com", phone: "+2347067411418", country: "Nigeria", accessCode: "2559SOM", unlockedVideos: {} },
     { name: "Chinetu Emmanuel", email: "chinetuemmanuel@gmail.com", phone: "+2349071375250", country: "Nigeria", accessCode: "1058CHI", unlockedVideos: {} },
    { name: "Micah Samuel", email: "micahblaq1@gmail.com", phone: "+31652560594", country: "Netherlands", accessCode: "5442BLA", unlockedVideos: {} },
@@ -13,7 +11,7 @@ let simulatedUserDatabase = [
  { name: "Ezumaibe Kelechiukwu", email: "sunshineworldcomputers@yahoo.com", phone: "+2349079244440", country: "Nigeria", accessCode: "2025ABC", unlockedVideos: {} },
  { name: "Peter obiakonwa", email: "peterobiakonwa@gmail.com", phone: "+2347048711326", country: "Nigeria", accessCode: "2001SHE", unlockedVideos: {} },
 { name: "Agalee Ifeanyi", email: "agaleeifeanyi@gmail.com", phone: "+2349057014199", country: "Nigeria", accessCode: "5555OKA", unlockedVideos: {} },
-{ name: "Agalee Ifeanyi", email: "dphilips1122@gmail.com", phone: "+2349057014199", country: "Nigeria", accessCode: "5555OKA", unlockedVideos: {} },
+{ name: "Denrele Philips", email: "dphilips1122@gmail.com", phone: "+2349076621945", country: "Nigeria", accessCode: "1414ABC", unlockedVideos: {} },
 
 ];
 // Initialize simulatedUserDatabase from localStorage if available
