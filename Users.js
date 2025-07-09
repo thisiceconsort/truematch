135e1876d96b6da7a056d9ad77fe404d548dffab
Mìlàájé <iceconsort@gmail.com>
2025-07-09 16:05:42 +0100

    Update Users.js

--- Users.js
+++ Users.js
@@ -1,23 +1,152 @@
     // Existing users - added subscriptionExpires for testing
 const hardcodedUsers = [
-    {
-        name: 'Aro Head',
-        email: 'aro@example.com',
-        phone: '+2348012345678',
-        country: 'Nigeria',
-        accessCode: '1234ARO',
-        unlockedVideos: [],
-        subscriptionExpires: new Date(Date.now() + 30 *  24 * 60 * 60 * 1000).toISOString()
-    },
+    let simulatedUserDatabase = [
     {
         name: 'John Doe',
-        email: 'john@example.com',
-        phone: '+1234567890',
-        country: 'USA',
-        accessCode: '1234ABC',
+        email: 'john.doe@example.com',
+        phone: '+2348012345678',
+        country: 'Nigeria',
+        accessCode: '3214JOH',
         unlockedVideos: [],
-        subscriptionExpires: new Date(Date.now() + 30 *  24 * 60 * 60 * 1000).toISOString()
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Sam Esom',
+        email: 'orjisamuel19@gmail.com',
+        phone: '+2347067411418',
+        country: 'Nigeria',
+        accessCode: '2559SOM',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Chinetu Emmanuel',
+        email: 'chinetuemmanuel@gmail.com',
+        phone: '+2349071375250',
+        country: 'Nigeria',
+        accessCode: '1058CHI',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Micah Samuel',
+        email: 'micahblaq1@gmail.com',
+        phone: '+31652560594',
+        country: 'Netherlands',
+        accessCode: '5442BLA',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Barnabas Chidindu',
+        email: 'chidindubarnabasiwuafor@gmail.com',
+        phone: '+2348066448413',
+        country: 'Nigeria',
+        accessCode: '1234BAN',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Emsley Izunna',
+        email: 'emsleyizunna@gmail.com',
+        phone: '+2348081848198',
+        country: 'Nigeria',
+        accessCode: '1234IZU',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Ezumaibe Kelechiukwu',
+        email: 'sunshineworldcomputers@yahoo.com',
+        phone: '+2349079244440',
+        country: 'Nigeria',
+        accessCode: '2025ABC',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Peter obiakonwa',
+        email: 'peterobiakonwa@gmail.com',
+        phone: '+2347048711326',
+        country: 'Nigeria',
+        accessCode: '2001SHE',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Agalee Ifeanyi',
+        email: 'agaleeifeanyi@gmail.com',
+        phone: '+2349057014199',
+        country: 'Nigeria',
+        accessCode: '5555OKA',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Denrele Philips',
+        email: 'dphilips1122@gmail.com',
+        phone: '+2349076621945',
+        country: 'Nigeria',
+        accessCode: '1414ABC',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Franklin Afola',
+        email: 'pe6970172@gmail.com',
+        phone: '+2349038116390',
+        country: 'Nigeria',
+        accessCode: '1997FRA',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Chidinma Iheanachor',
+        email: 'lightsolomon210eli@gmail.com',
+        phone: '+2348128678246',
+        country: 'Nigeria',
+        accessCode: '6754IKE',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Onyeka Arimonu',
+        email: 'omegachris@gmail.com',
+        phone: '+23481342..725',
+        country: 'Nigeria',
+        accessCode: '1345ADC',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Osmond Echezona',
+        email: 'echezonaosmond@gmail.com',
+        phone: '+2348061452349',
+        country: 'Nigeria',
+        accessCode: '5602ECH',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Modestus Chukwudalu',
+        email: 'echezonaosmond@gmail.com',
+        phone: '+2348061452349',
+        country: 'Nigeria',
+        accessCode: '2349ECH',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    {
+        name: 'Emmanuel Edet',
+        email: 'okoney12@gmail.com',
+        phone: '+2348121272626',
+        country: 'Nigeria',
+        accessCode: '1234OKO',
+        unlockedVideos: [],
+        subscriptionExpires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toISOString()
     }
+];
+
     // Add more here
 ];
 
