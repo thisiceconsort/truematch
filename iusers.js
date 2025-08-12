248c1ed7ef72f3a0d7aa0cd418175e95a1dbb229
Mìlàájé <iceconsort@gmail.com>
2025-08-12 22:59:40 +0100

    Update iusers.js

--- iusers.js
+++ iusers.js
@@ -119,7 +119,7 @@ const hardcodedUsers = [
         suspended: 'F', 
         subscription: 'T', 
         subscriptionExpiry: new Date(new Date().setMonth(new Date().getMonth() + 1)).toISOString(), 
-        availableBalance: 9000.00,
+        availableBalance: 9500.00,
         referralBalance: 0.00,
         avatar: 'https://via.placeholder.com/150/007bff/FFFFFF?text=AA'
     },
