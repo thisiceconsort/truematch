7ddca5b8dee9ec90981442f30e8025e5c8bf3566
Mìlàájé <iceconsort@gmail.com>
2025-08-31 13:05:59 +0100

    Update iusers.js

--- iusers.js
+++ iusers.js
@@ -308,7 +308,7 @@ const hardcodedUsers = [
         suspended: 'F', 
         subscription: 'T', 
         subscriptionExpiry: new Date(new Date().setMonth(new Date().getMonth() + 1)).toISOString(), 
-        availableBalance: 10500.00,
+        availableBalance: 11250.00,
         referralBalance: 0.00,
         avatar: 'https://dl.dropboxusercontent.com/scl/fi/ucb2qkebfqw1xn1tdi7qm/Screenshot_2025-08-28-11-40-53-486_org.telegram.messenger-2.jp'
     },
